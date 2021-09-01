6bbd667f929a728c41cb870597c4a19c298ddcf1
Stefan Dragicevic <stefandragicevic@Stefan-Dragicevic.local>
2021-09-01 14:04:00 +0200

    Init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '68a62701d8f85eda906f84f27835956a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a1dd587d-5762-40cf-9c14-93843f8f37e3'
+};
